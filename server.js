b71dba0a6b151dda351dcf2b62809decbdf6e8e2
mbgoseco <mbgoseco@gmail.com>
2018-12-03 14:09:19 -0800

    possible fix to heroku deployment

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('../portfolio'));
+app.use(express.static('./'));
 
 app.get('/home', function(req, res) {
   res.sendFile(`${__dirname}/index.html`);
